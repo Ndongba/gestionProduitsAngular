385849870451925f3f28c4a0be6aa86b214f6f88
ndongba <ndongba01@gmail.com>
2024-09-04 16:20:28 +0000

    fonctionnalité ajout de produit

---
+++ src/app/components/create-produit/create-produit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-produit',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-produit.component.html',
+  styleUrl: './create-produit.component.css'
+})
+export class CreateProduitComponent {
+
+}
